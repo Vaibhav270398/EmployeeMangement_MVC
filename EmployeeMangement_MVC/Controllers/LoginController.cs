c9520fcd2abdc60b710528200831c77a5c2acac2
vaibhav bhosale <vaibhavbhosale270398@gmail.com>
2025-02-10 23:05:57 +0530

    Add project files.

---
+++ EmployeeMangement_MVC/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeMangement_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeMangement_MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginModel loginModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ EmployeeMangement_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeMangement_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
